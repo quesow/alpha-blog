ce84018bc541f3dc2a989d86e3771de2b1a4a253
JESUA BETANCOR ALEMAN <JBETANCOR.0011@GMAIL.COM>
2018-11-07 18:01:41 +0100

    add association between users and articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -11,6 +11,7 @@ class ArticlesController < ApplicationController
 
   def create
     @article = Article.new(article_params)
+    @article.user = User.first # temporary solution
     if @article.save
       flash[:success] = "Article was successfully created"
       redirect_to article_path(@article)
@@ -26,6 +27,7 @@ class ArticlesController < ApplicationController
   end
 
   def update
+    @article.user = User.first # temporary solution
     if @article.update(article_params)
       flash[:success] = "Article was successfully updated"
       redirect_to article_path(@article)
