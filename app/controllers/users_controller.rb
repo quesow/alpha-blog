5aecde2ce3933efb2eb1f9645f765c6f37526c11
JESUA BETANCOR ALEMAN <JBETANCOR.0011@GMAIL.COM>
2018-11-12 17:38:28 +0100

    wrapping up user views

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -20,7 +20,7 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     if @user.update(user_params)
       flash[:success] = "Your account was updated successfully, #{@user.username}!"
-      redirect_to articles_path
+      redirect_to users_path
     else
       render 'edit'
     end
@@ -28,6 +28,10 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
+  end
+
+  def index
+    @users = User.all
   end
 
   private
