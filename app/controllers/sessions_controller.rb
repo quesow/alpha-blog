49f706d21384d292632250165435426c4589e61b
JESUA BETANCOR ALEMAN <JBETANCOR.0011@GMAIL.COM>
2018-11-13 19:48:53 +0100

    add sessions and auth methods

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,27 @@
+class SessionsController < ApplicationController
+  def new
+  end
+
+  def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      session[:user_id] = user.id
+      flash[:success] = "You're successfully logged in, #{user.username}!"
+      redirect_to user_path(user)
+    else
+      flash[:danger] = "There was something wrong with your login information."
+      render 'new'
+    end
+  end
+
+  def destroy
+    session[:user_id] = nil
+    flash[:success] = "You have logged out"
+    redirect_to root_path
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:email, :password)
+  end
+end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:edit, :update, :show]
   def new
     @user = User.new
   end
@@ -13,11 +14,9 @@ class UsersController < ApplicationController
   end
 
   def edit
-    @user = User.find(params[:id])
   end
 
   def update
-    @user = User.find(params[:id])
     if @user.update(user_params)
       flash[:success] = "Your account was updated successfully, #{@user.username}!"
       redirect_to users_path
@@ -27,7 +26,6 @@ class UsersController < ApplicationController
   end
 
   def show
-    @user = User.find(params[:id])
     @user_articles = @user.articles.paginate(:page => params[:page], :per_page => 3)
   end
 
@@ -38,5 +36,8 @@ class UsersController < ApplicationController
   private
   def user_params
     params.require(:user).permit(:username, :email, :password)
+  end
+  def set_user
+    @user = User.find(params[:id])
   end
 end
