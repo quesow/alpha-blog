8d3cd4a1762cc9125cf9a16a8d7625452aa53265
JESUA BETANCOR ALEMAN <JBETANCOR.0011@GMAIL.COM>
2018-11-21 17:00:38 +0100

    Add admin user functionality

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_10_134054) do
+ActiveRecord::Schema.define(version: 2018_11_21_152956) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -24,6 +24,7 @@ ActiveRecord::Schema.define(version: 2018_11_10_134054) do
     t.string "username"
     t.string "email"
     t.string "password_digest"
+    t.boolean "admin", default: false
   end
 
 end
